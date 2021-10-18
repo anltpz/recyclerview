4e0fca912baebdfd10a638e1f1bd68ee495be6f8
Anıl TOPUZ <aniltopuz@gmail.com>
2021-10-19 01:50:20 +0300

    RecyclerView Eklendi

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -10,12 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
-        //nerhaba
-        // git push
-        // git push  2
 
-        // main ekleme yapıldı 3
 
 
         
